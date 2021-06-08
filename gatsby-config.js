75b9f5fec836eeede98832101454876461d97759
DangDev <dream858vip@gmail.com>
2021-06-08 12:57:03 +0700

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yellowcake',
-    siteUrl: 'https://yellowcake.netlify.com'
+    title: 'Nguyễn Hải Đăng',
+    siteUrl: 'https://DangDev.github.io'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
