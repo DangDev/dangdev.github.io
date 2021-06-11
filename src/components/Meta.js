487dd326d2bbadafa825e2c10d509e48fa9e6f44
DangDev <dream858vip@gmail.com>
2021-06-11 13:40:01 +0700

    update css

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -63,6 +63,10 @@ export default class Meta extends Component {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         />
+        <link
+          rel="stylesheet"
+          href="https://ghcdn.rawgit.org/DangDev/all-files/main/click-effect.css"
+        ></link>
         <script src="https://ghcdn.rawgit.org/DangDev/all-files/main/cursor-effect.js"></script>
         <script src="https://ghcdn.rawgit.org/DangDev/all-files/main/click-effect.js"></script>
 
